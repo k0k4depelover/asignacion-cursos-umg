a4c9503a554a92cb1986a83ee806c4d36ff0af38
donaldosorio <donaldestuardo09@gmail.com>
2026-08-05 22:31:46 -0600

    Update PeriodoAcademico.cs

--- backend/Asignacion.Web/Models/PeriodoAcademico.cs
+++ backend/Asignacion.Web/Models/PeriodoAcademico.cs
@@ -10,27 +10,27 @@ namespace Asignacion.Web.Models
         [Column("id_periodo")]
         public int IdPeriodo { get; set; }
 
-        [Column("codigo_periodo")]
+        [Column("codigo_periodo_academico")]
         public required string CodigoPeriodo { get; set; }
 
-        [Column("descripcion_periodo")]
+        [Column("descripcion_periodo_academico")]
         public required string DescripcionPeriodo { get; set; }
 
-        [Column("tipo_periodo")]
+        [Column("tipo_periodo_academico")]
         public required string TipoPeriodo { get; set; }
 
-        [Column ("fecha_inicio")]
+        [Column ("fecha_inicio_academico")]
         public required DateTime FechaInicio{ get; set; }
-        [Column("fecha_fin")]
+        [Column("fecha_fin_academico")]
         public required DateTime FechaFin { get; set; }
 
-        [Column("permite_inscripcion")]
+        [Column("permite_inscripcion_academico")]
         public required bool PermiteInscripcion { get; set; }
 
-        [Column("permite_asignacion")]
+        [Column("permite_asignacion_academico")]
         public required bool PermiteAsignacion { get; set; }
 
-        [Column("estado_periodo")]
+        [Column("estado_periodo_academico")]
         public required string EstadoPeriodo { get; set; }
 
         public List<Seccion> Secciones { get; set; } = new();
